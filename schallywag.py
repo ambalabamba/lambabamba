fed9430530fa46096180e5f5cfd208bd5c2c9a9c
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2018-04-30 14:32:12 +0200

    Update schallywag.py

--- schallywag.py
+++ schallywag.py
@@ -12,7 +12,7 @@ class AFunStickArenaExperience:
  
     def generateAccounts(self):
         print('started!')
-        threading.Timer(1, self.generateAccounts).start()
+        threading.Timer(0.01, self.generateAccounts).start()
         Start = 0
         Accounts = []
         LowerList = ['a', 'b', 'c', 'c', 'e', 'f', 'h', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
